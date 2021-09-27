76743fe0e91e6df3a9816a3017164af05c48abfe
Gabriel <gabrielfernandesfg@hotmail.com>
2021-09-27 01:06:56 -0300

    feature: base repositories and users CURD

--- src/shared/utils/userHasPermission.ts
+++ src/shared/utils/userHasPermission.ts
@@ -1,14 +1,15 @@
 import { flatten } from '@nestjs/common';
 import { removeDuplicates } from './array';
-import { Permission, User, Role } from '@prisma/client';
 
 export default function userHasPermission(
     user: any,
     requiredPermissions: string[],
 ) {
     if (user !== undefined) {
-        const permissions: Permission[] = removeDuplicates(
-            flatten(user?.roles.map((role: any) => role?.permissions)) as any[],
+        const permissions: any[] = removeDuplicates(
+            flatten(
+                user?.roles?.map((role: any) => role?.permissions),
+            ) as any[],
         );
 
         return permissions.some((permission) =>
